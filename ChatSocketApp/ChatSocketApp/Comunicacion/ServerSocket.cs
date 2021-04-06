c39319a485da4f860f182425ca3da16a06a53273
Benjamin Basaez <benjamin.ibm@gmail.com>
2021-04-06 17:04:47 -0400

    Agrega Servidor Socket

---
+++ ChatSocketApp/ChatSocketApp/Comunicacion/ServerSocket.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatSocketApp.Comunicacion
+{
+    public class ServerSocket
+    {
+        private int puerto;
+        private Socket servidor;
+        private Socket comCliente;
+        private StreamWriter writer;
+        private StreamReader reader;
+        /// <summary>
+        /// El servidor se levanta en el puerto especifico
+        /// </summary>
+        /// <param name="puerto"></param>
+
+
+        public ServerSocket(int puerto)
+        {
+            this.puerto = puerto;
+            
+        }
+
+
+        public bool Iniciar()
+        {
+
+            try
+            {
+            //Crear instancia de socket
+            this.servidor = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            //Hacer bind para tomar control del puerto
+            this.servidor.Bind(new IPEndPoint(IPAddress.Any, this.puerto));
+            //Escuchar a una cantidad de clientes
+            this.servidor.Listen(10);
+
+            return true;
+
+            }catch(Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+        public bool Escribir(string mensaje)
+        {
+            try
+            {
+                this.writer.WriteLine(mensaje);
+                this.writer.Flush();
+                return true;
+            }catch(Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public string Leer()
+        {
+            try
+            {
+                return this.reader.ReadLine().Trim();
+            }catch(Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public bool ObtenerCliente()
+        {
+            try
+            {
+                this.comCliente = this.servidor.Accept();
+                Stream stream = new NetworkStream(this.comCliente);
+                this.writer = new StreamWriter(stream);
+                this.reader = new StreamReader(stream);
+
+                return true;
+            }catch(Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public void CerrarConexion()
+        {
+            this.comCliente.Close();
+        }
+    }
+}
--- ChatSocketApp/ChatSocketApp/Program.cs
+++ ChatSocketApp/ChatSocketApp/Program.cs
@@ -1,15 +1,52 @@
-﻿using System;
+﻿using ChatSocketApp.Comunicacion;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ChatSocketApp
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
+            ServerSocket serverSocket = new ServerSocket(puerto);
+            if (serverSocket.Iniciar())
+            {
+                //Puedo esperar un cliente
+                while (true)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Esperando cliente...");
+                    if (serverSocket.ObtenerCliente())
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("Cliente recibido!");
+                        Console.WriteLine("S: Hola CLiente");
+                        serverSocket.Escribir("Hola CLiente");
+                        //Mensaje hacia el cliente
+                        Console.WriteLine("Escriba un mensaje al cliente");
+                        string mensaje = Console.ReadLine().Trim();
+                        Console.WriteLine("S:{0}", mensaje);
+                        serverSocket.Escribir(mensaje);
+
+                        string respuesta = serverSocket.Leer();
+                        Console.WriteLine("C:{0}", respuesta);
+
+                        serverSocket.CerrarConexion();
+                    }
+                }
+            }
+            else
+            {
+                //Por permisos no puedo levantar la comunicacion en el puerto
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("No se puede levantar el servidor, puerto ocupado o sin privilegios");
+                Console.ReadKey();
+            }
         }
     }
 }
